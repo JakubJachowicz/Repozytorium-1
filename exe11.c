ab947d645d81c048220b3696a8b862918158057c
JakubJachowicz <kuba713jach@gmail.com>
2017-04-25 01:31:39 +0200

    Commit #20

---
+++ exe11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int tablica[8];
+    int i;
+    tablica[0] = 2;
+    for(i=1;i<8;i++)
+    {
+        tablica[i] = 2*tablica[i-1];
+    }
+    i=0;
+    do
+    {
+        printf("%d ", tablica[i]);
+        i++;
+    }while(i<=7);
+    printf("\n");
+    return 0;
+}
