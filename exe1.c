ffcdd7dc419c088e47ab694757dd223a4d0a7529
JakubJachowicz <kuba713jach@gmail.com>
2017-04-25 01:22:13 +0200

    Commit #11

--- exe1.c
+++ exe1.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#define MIN_W_GODZ   60
+
+const int ArSize = 26;
 
 int main()
 {
-    int czas = 0;
-    int godziny, minuty;
-
-    while(czas++ < 1439)
+    char tablica[ArSize];
+    int i;
+    char ch;
+    printf("Podaj 26 znakow:\n");
+    for (i=0;i<ArSize;i++)
     {
-        godziny = czas/MIN_W_GODZ;
-        minuty = czas%MIN_W_GODZ;
-        printf("%d:%d\n", godziny, minuty);
+        scanf("%c", &ch);
+        tablica[i] = ch;
+    }
+    printf("Te znaki to:\n");
+    for(i=0;i<ArSize;i++)
+    {
+        printf("%c ", tablica[i]);
     }
     return 0;
 }
