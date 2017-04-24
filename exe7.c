6e2ff179be0e7abae66c84f43688ae291993d952
JakubJachowicz <kuba713jach@gmail.com>
2017-04-25 01:09:24 +0200

    Commit #7
    Signed-off-by: JakubJachowicz <kuba713jach@gmail.com>

---
+++ exe7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float f1, f2;
+
+    printf("Podaj dwie liczby zmiennoprzecinkowe: ");
+    while(scanf("%f %f", &f1, &f2) == 2)
+    {
+        printf("Stosunek roznicy tych liczb do ich iloczynu wynosi: %f", (f1-f2)/(f1*f2));
+    }
+    printf("Koniec!\n");
+    return 0;
+}
