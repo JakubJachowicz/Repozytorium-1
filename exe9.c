2f256c034ab8a5b36433f5cc3606ed38809ddb43
JakubJachowicz <kuba713jach@gmail.com>
2017-04-25 01:11:11 +0200

    Commi #9
    Signed-off-by: JakubJachowicz <kuba713jach@gmail.com>

---
+++ exe9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int liczby[8];
+    int i;
+
+    printf("Podaj osiem liczb calkowitych:\n");
+    for (i=0;i<8;i++)
+    {
+        scanf("%d", &liczby[i]);
+    }
+    printf("Te liczby w odwrotnej kolejnosci to:\n");
+    for (i=7;i>=0;i--)
+    {
+        printf("%d ", liczby[i]);
+    }
+    printf("\n");
+    return 0;
+}
