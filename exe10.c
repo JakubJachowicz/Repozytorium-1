efdc9d6fb1bad5a2cbf117a21f90fca6b3442ebf
JakubJachowicz <kuba713jach@gmail.com>
2017-04-25 01:11:41 +0200

    Commit #10
    Signed-off-by: JakubJachowicz <kuba713jach@gmail.com>

---
+++ exe10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float limit, suma, i;
+
+    printf("Podaj liczbe, dla jakiej program ma sumowac kolejne ulamki 1/n: ");
+    scanf("%f", &limit);
+    for (i=1.0;i<=limit;i += 1.0)
+    {
+        suma += 1/i;
+    }
+    printf("Suma %d wyrazow tego ciagu wynosi: %f.\n", (int)limit, suma);
+    return 0;
+}
