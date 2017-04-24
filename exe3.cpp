b796bf83939027c814b7449533239941ea5352b1
JakubJachowicz <kuba713jach@gmail.com>
2017-04-25 01:24:42 +0200

    Commit #13

---
+++ exe3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char i = 'F';
+    char j;
+    for(i;i>='A';i--)
+    {
+        for(j='F';j>=i;j--)
+        {
+            printf("%c", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
