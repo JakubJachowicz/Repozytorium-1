4b5a67d469c128086bf2bf0e16508b59ab26dbea
JakubJachowicz <kuba713jach@gmail.com>
2017-04-25 01:46:53 +0200

    Commit #27

--- exe10.c
+++ exe10.c
@@ -4,6 +4,7 @@ int main()
 {
     float limit, suma, i;
 
+
     printf("Podaj liczbe, dla jakiej program ma sumowac kolejne ulamki 1/n: ");
     scanf("%f", &limit);
     for (i=1.0;i<=limit;i += 1.0)
