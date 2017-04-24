fd458331c96bf399481b2bdda737e05cc30e1d79
JakubJachowicz <kuba713jach@gmail.com>
2017-04-25 01:30:10 +0200

    Commit #18

---
+++ exe13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    double Ewa, Kasia;
+    int rok;
+    rok = 1;
+    Ewa = Kasia = 100.0;
+
+    while(Kasia <= Ewa)
+    {
+        printf("Rok: %2d  Stan konta Ewy: %4.2f   Stan konta Kasi: %4.2f\n", rok, Ewa, Kasia);
+        Ewa += 10.0;
+        Kasia *= 1.05;
+        rok++;
+    }
+    return 0;
+}
