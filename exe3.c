e54ddd437ffd7e2b13269ec72f7cfc63531a17ed
JakubJachowicz <kuba713jach@gmail.com>
2017-04-25 01:03:09 +0200

    Commit #3
    Signed-off-by: JakubJachowicz <kuba713jach@gmail.com>

---
+++ exe3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define DAYS_PER_WEEK   7
+
+int main()
+{
+    int dni, tygodnie;
+
+    printf("Podaj dowolna liczbe dni: ");
+    scanf("%d", &dni);
+    tygodnie = dni/DAYS_PER_WEEK;
+    dni = dni%DAYS_PER_WEEK;
+    printf("\nTo inaczej %d tygodni i %d dni.\n", tygodnie, dni);
+    return 0;
+}
